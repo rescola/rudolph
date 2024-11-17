7687688ae937cf18613644c8adb770d69ccebc3a
Roger Escolà <rogeresbe@gmail.com>
2024-11-18 00:43:01 +0100

    Logica porta y canvi escena

---
+++ Assets/Door_juguetes.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;  // Per canviar escenes
+
+public class DoorController : MonoBehaviour
+{
+    public Sprite closedDoorSprite; 
+    public Sprite openDoorSprite;   
+
+    public AudioClip openDoorSound; // revisar audio ja que no va
+    public AudioClip closeDoorSound;
+
+    // Estat porta
+    private bool isDoorOpen = false; // Indica si la porta esta oberta/tancada
+
+    // Distancia d'interaccio
+    public float interactionDistance; // Distancia per iteracturar amb la porta
+
+    private SpriteRenderer spriteRenderer;
+    private AudioSource audioSource;
+    private GameObject player;
+    private Posicionador playerMovement;  // Referencia al script de moviment
+
+    private Vector3 targetPosition;  // Posicio a la que s'ha de moure el player
+
+    private void Start()
+    {
+        // Components necesaris
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
+
+        // Configurar sprite inicial com porta tancada
+        spriteRenderer.sprite = closedDoorSprite;
+
+        // Obtenir el objecte del jugador
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        // Obtenir el script de moviment del jugador
+        playerMovement = player.GetComponent<Posicionador>();
+    }
+
+    private void Update()
+    {
+        // Clic dret del mouse
+        if (Input.GetMouseButtonDown(1))
+        {
+            // Comproba si el jugador esta aprop de la porta cuan fa clic dret
+            if (Vector2.Distance(player.transform.position, transform.position) <= interactionDistance)
+            {
+                ToggleDoor();  // Si esta aprop obra / tanca directament
+            }
+            else
+                // Nomes es moura si el click esta prop de la porta
+                if (Vector2.Distance(Camera.main.ScreenToWorldPoint(Input.mousePosition), transform.position) <= interactionDistance)
+            {
+                // Si esta lluny es moura cap a la porta
+                targetPosition = transform.position;
+                Debug.Log("targetPosition crida: " + targetPosition);
+                playerMovement.SetTargetPosition(targetPosition, this);  // Mou el player
+            }
+        }
+        // Clic esquerra
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Si la porta esta oberta i fa click esquerra
+            if (isDoorOpen && Vector2.Distance(player.transform.position, transform.position) <= interactionDistance)
+            {
+                // Camvia l'escena
+                SceneManager.LoadScene("Sala_juguetes");
+            }
+        }
+    }
+
+    public void ToggleDoor()
+    {
+        // Cambia estat porta
+        isDoorOpen = !isDoorOpen;
+
+        // Actualiza el sprite segon el estat de la porta i fa soroll
+        if (isDoorOpen)
+        {
+            spriteRenderer.sprite = openDoorSprite;
+            PlaySound(openDoorSound);
+        }
+        else
+        {
+            spriteRenderer.sprite = closedDoorSprite;
+            PlaySound(closeDoorSound);
+        }
+    }
+
+    private void PlaySound(AudioClip clip)
+    {
+        if (clip != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(clip); // Reprodueix el so una vegada
+        }
+    }
+}
--- Assets/posicionador.cs
+++ Assets/posicionador.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class posicionador : MonoBehaviour
+public class Posicionador : MonoBehaviour
 {
     public float velocidad = 1f;
     private Vector3 objetivo;
     private Animator animator;
+    private bool door = false;
+    private DoorController currentDoor; // Referencia a la porta actual
+
+    public float maxY; // Limit de les Y (això soluciona problemes si jugador marca una paret)
+    public float distancia; //Distancia entre personatge y punt marcat
+
 
     void Start()
     {
         objetivo = transform.position;
-
         animator = GetComponent<Animator>();
     }
 
@@ -20,32 +25,55 @@ public class posicionador : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             objetivo = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            objetivo.z = 0;
+
+            if (objetivo.y > maxY)
+            {
+                objetivo.y = maxY;
+            }
+
+            objetivo.z = 0; // Seguretat de les Z (ha de ser 0)
         }
 
+        // Mou jugador cap al objectiu
         float distanciaAlObjeto = Vector3.Distance(transform.position, objetivo);
-
-
-        transform.position = Vector3.MoveTowards(transform.position, objetivo, velocidad * Time.deltaTime);
-
-
-        if (distanciaAlObjeto > 0.2f)
+        if (distanciaAlObjeto > distancia)
         {
+            transform.position = Vector3.MoveTowards(transform.position, objetivo, velocidad * Time.deltaTime);
             animator.SetBool("isMoving", true);
 
+            // Direcció del personatge
             if (objetivo.x > transform.position.x)
             {
-                transform.localScale = new Vector3(1, 1, 1); // mou a la dreta
+                transform.localScale = new Vector3(1, 1, 1); // Mou a la dreta
             }
             else
             {
-                transform.localScale = new Vector3(-1, 1, 1); // mou a la dreta invertint imatge
+                transform.localScale = new Vector3(-1, 1, 1); // Mou a l'esquerra
             }
         }
         else
         {
-            animator.SetBool("isMoving", false);
+            animator.SetBool("isMoving", false); // Es para
+            if (door == true && currentDoor != null)
+            {
+                currentDoor.ToggleDoor(); //Crida al metode per obrir/tancar la porta
+                door = false; // resetea l'estat
+                currentDoor = null; // reseteja la referencia
+            }
         }
+    }
 
+    // Mètode públic per canviar l'objectiu de moviment (utilitzat en DoorController)
+    public void SetTargetPosition(Vector3 newTarget, DoorController doorController)
+    {
+        objetivo = newTarget;
+        if (objetivo.y > maxY)
+        {
+            objetivo.y = maxY;
+        }
+        objetivo.z = 0;
+        Debug.Log("targetPosition final: " + objetivo);
+        door = true;
+        currentDoor = doorController; // Asigna la referencia de la porta
     }
 }
