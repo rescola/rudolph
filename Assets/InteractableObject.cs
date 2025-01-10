fa3dcbd81ea2e6f70715fa6056c38e330f89d27c
Roger Escolà <rogeresbe@gmail.com>
2025-01-10 11:05:01 +0100

    prevé recollir objectes infinits

--- Assets/InteractableObject.cs
+++ Assets/InteractableObject.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
-    public string objectName = "Objeto"; // Nom per defecte en cas que em deixi algun
+    public string objectName = "Objete"; // Nom per defecte en cas que em deixi algun
     public InventoryItem itemToAdd; // Objecte que s'agefirà a l'inventari
     public string interactionMessage; // Missatge que dira el personatge
+    public string emptyMessage = ""; // Missatge després de recollir l'objecte
+
+    private bool hasBeenCollected = false; // Estat de l'objecte: recollit o no
 
     private void OnMouseOver()
     {
@@ -16,6 +19,16 @@ public class InteractableObject : MonoBehaviour
 
     private void Interact()
     {
+        if (hasBeenCollected)
+        {
+            // Mostrar missatge si l'objecte ja ha estat recollit
+            if (!string.IsNullOrEmpty(emptyMessage))
+            {
+                Debug.Log(emptyMessage);
+                MessageManager.Instance.ShowMessage(emptyMessage, 3f);
+            }
+            return; // Finalitza aquí si l'objecte ja està recollit
+        }
         if (!string.IsNullOrEmpty(interactionMessage))
         {
             Debug.Log(interactionMessage); // Missatge del personatge
@@ -25,6 +38,7 @@ public class InteractableObject : MonoBehaviour
         {
             InventoryManager.Instance.AddItemToInventory(itemToAdd); // posa l'intem a l'inventari
         }
+        hasBeenCollected = true; // Marca l'objecte com recollit
 
 
     }
